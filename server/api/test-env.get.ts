c566e95df5bf1b013f588cb71ce1a1d42af8fb90
mike_banner <mike.webfree@gmail.com>
2026-06-08 19:14:40 +0200

    chore: test full env vars

--- server/api/test-env.get.ts
+++ server/api/test-env.get.ts
@@ -1,8 +1,8 @@
 export default defineEventHandler((event) => {
   const config = useRuntimeConfig(event)
   return {
-    r2AccountId: config.r2AccountId ? 'SET' : 'NOT_SET',
-    r2AccountIdValue: config.r2AccountId,
-    r2BucketName: config.r2BucketName
+    r2AccountId: config.r2AccountId,
+    r2AccessKeyId: config.r2AccessKeyId,
+    r2SecretAccessKey: config.r2SecretAccessKey
   }
 })
